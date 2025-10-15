6cbade65dd0af8a5c6d425fb86eb58f8765069e0
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-10-15 21:54:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("welcome from the console");
+
+// alert("welcome to Rabat");
+
+// prompt("put your number :");
+
+// confirm("do you need to close it?");
+
+// Data Types **************************************************
+var x = 10;
+var y = 20;
+var z;
+
+z = x;
+x = y;
+y = z;
+
+console.log("la valeur de x est : ", x);
+console.log("la valeur de y est : ", y);
