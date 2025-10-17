5285718b4cc81288e51e19266da97320e10370ad
ELMACHHOUNE <mohamed20rida@gmail.com>
2025-10-17 22:07:36 +0100

    refactor: comment out unused variables and console logs in script.js

--- script.js
+++ script.js
@@ -7,13 +7,67 @@
 // confirm("do you need to close it?");
 
 // Data Types **************************************************
-var x = 10;
-var y = 20;
-var z;
+// var x = 10;
+// var y = 20;
+// var z;
 
-z = x;
-x = y;
-y = z;
+// z = x;
+// x = y;
+// y = z;
 
-console.log("la valeur de x est : ", x);
-console.log("la valeur de y est : ", y);
+// console.log("la valeur de x est : ", x);
+// console.log("la valeur de y est : ", y);
+
+// let mohamed = 20;
+
+// const p = 3.14;
+
+// console.log(p);
+
+// var rachid = "rachid bakkass";
+// console.log(rachid);
+
+// var rachid = "bakkass";
+
+// console.log(rachid);
+
+// var x = "20";
+// var y = "20";
+// var z = 30;
+// console.log("la valeur de X est :", x, "et le type de X est :", typeof x);
+// console.log("la valeur de Y est :", y, "et le type de Y est :", typeof y);
+
+// console.log(x + y);
+
+// var x = NaN;
+// var y = null;
+
+// console.log(typeof y);
+
+//Objects : ****************************************
+// var Person1 = {
+//   nom: "mohamed",
+//   prenom: "el machhoune",
+//   age: 27,
+//   isStudent: false,
+//   cours: {
+//     frontend: "React",
+//     backend: "ExpressJS",
+//   },
+// };
+
+// console.log(Person1.nom, Person1.prenom);
+// console.log(Person1.cours);
+
+//arrays : ****************************************
+// hala = 21;
+// var Person2 = [true, false, 20, "simo", hala, 30];
+
+// console.log(Person2[3]);
+// console.log(Person2.length);
+// console.log(Person2[4]);
+
+// var x = Person2[2];
+// var y = Person2[5];
+// var z = x + y;
+// console.log(z);
